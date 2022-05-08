756264442b534137b074d8e6850f39b09c1b092e
tesar-tech <jan@tesar.tech>
2022-05-08 10:43:37 +0200

    app settings

--- 12/PptNemocnice/PptNemocnice/Program.cs
+++ 12/PptNemocnice/PptNemocnice/Program.cs
@@ -6,8 +6,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7212") });
+var baseAddress = builder.Configuration["HttpClientBaseAddress"];
+if (baseAddress is null) throw new ArgumentNullException(paramName: nameof(baseAddress), message:"base address for api communication cannot be null") ;
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
 
 await builder.Build().RunAsync();
 
